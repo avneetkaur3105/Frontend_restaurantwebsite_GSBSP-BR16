4227f3a181770ba6ee988361d436d1ac155a4958
avneetkaur3105 <46818035+avneetkaur3105@users.noreply.github.com>
2020-07-18 21:16:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var i=0,text;
+text="Delightful Treat"
+function typing(){
+    if(i<text.length){
+        document.getElementById("text").innerHTML+=text.charAt(i);
+        i++;
+        setTimeout(typing,150);
+
+    }
+}
+
+typing()
